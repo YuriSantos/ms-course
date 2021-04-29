199754cb3f93cc2dd9be6a478fd3b2dd40aab4a6
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2021-04-29 14:17:07 -0300

    Hr-Worker implementation

---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplicacion.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplicacion {
+	
+	public static void main(String[] args) {
+			SpringApplication.run(HrWorkerApplicacion.class, args);
+	}
+
+}
