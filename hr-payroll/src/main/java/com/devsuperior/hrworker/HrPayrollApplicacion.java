f70d5b05733630b7a288981306219add614b95dd
Yuri Barbosa Santos <ybarbosa@indracompany.com>
2021-05-13 18:10:28 -0300

    Adicionando o payment e os serviços de consulta do mesmo

---
+++ hr-payroll/src/main/java/com/devsuperior/hrworker/HrPayrollApplicacion.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollApplicacion {
+	
+	public static void main(String[] args) {
+			SpringApplication.run(HrPayrollApplicacion.class, args);
+	}
+
+}
